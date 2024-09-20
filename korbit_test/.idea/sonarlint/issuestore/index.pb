1ced85ffaf0b023edc233cd4026facb5692e3031
Pushpendra Chauhan <pushpendra.chauhan@managecaptive.com>
2024-09-20 10:30:29 +0530

    test

---
+++ korbit_test/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+A
+src/Students.java,7\f\7f785c419e56dd6790a6148c4980274ea8336e5a
---
+++ korbit_test/src/Students.java
@@ -0,0 +1,21 @@
+public class Students {
+
+    private  String name;
+    private String addres;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+}
